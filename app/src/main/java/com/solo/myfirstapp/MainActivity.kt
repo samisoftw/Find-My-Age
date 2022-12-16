9f74c54ce77f4b94990c605ba5cf8a0822c54fc3
samisoftw <hadjsoft@gmail.com>
2022-12-16 20:26:04 +0000

    UI updated

--- app/src/main/java/com/solo/myfirstapp/MainActivity.kt
+++ app/src/main/java/com/solo/myfirstapp/MainActivity.kt
@@ -2,6 +2,7 @@ package com.solo.myfirstapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 import java.util.*
 
@@ -12,10 +13,17 @@ class MainActivity : AppCompatActivity() {
 
         BtnOutput.setOnClickListener{
             val UserInput:Int= Integer.parseInt(DataInput.text.toString())
+            val UserInput1 =DataInput.text.toString()
             val currentYear:Int= Calendar.getInstance().get(Calendar.YEAR)
             val UserAge= currentYear - UserInput
-            DataOutput.text="Your age is: $UserAge Years"
+            if(UserInput>currentYear || UserInput==0) {DataOutput.text ="error try again" }
+            else if(UserInput==currentYear ) {DataOutput.text="Your age is: less than 1 Year" }
+            else if(UserInput1 =="") { Toast.makeText(this,"Please enter birth year",Toast.LENGTH_SHORT).show() }
+            else {DataOutput.text="Your age is: $UserAge Years"}
+            }
 
         }
     }
-}
+
+
+
